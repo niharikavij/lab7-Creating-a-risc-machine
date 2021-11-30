dcb3090e563be6fcafb6200b364476c56751ee65
niharikavij <89627495+niharikavij@users.noreply.github.com>
2021-11-29 16:23:39 -0800

    Update lab7_top.v

--- lab7_top.v
+++ lab7_top.v
@@ -4,7 +4,7 @@ input [9:0] SW;
 output [9:0] LEDR;
 output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 
-  RAM MEM(.clk(~KEY[0]), .read_address(SW), .write_address(SW), //to finish
+  RAM MEM(.clk(~KEY[0]), .read_address(SW), .write_address(SW)) //to finish
 
 
 
@@ -33,3 +33,32 @@ module RAM(clk,read_address,write_address,write,din,dout);
                                // (this is due to Verilog non-blocking assignment "<=")
   end 
 endmodule
+ 
+module PC(next_pc, pc_out, load_pc); // logic for program counter
+  input [8:0] next_pc;
+  input load_pc;
+  output wire [8:0] pc_out;
+  
+  always@(posedge clk) begin //dependent on clock
+   pc_out = (load_pc) ? next_pc: pc_out;
+          
+endmodule
+
+module Mux_pc(pc_out, reset_pc, next_pc); // logic for multiplexer before the PC
+  input [8:0] pc_out;
+  input reset_pc;
+  output wire [8:0] next_pc;
+  
+  assign next_pc = (reset) ? 8'b00000000 : (out_pc + 1'b1) ; // when reset  = 0 , value going int0 PC is incremented, such that PC stores address of next instruction
+
+endmodule
+
+module Mux_memory(pc_out, addr_sel, mem_addr);
+  input [8:0] pc_out;
+  input addr_sel;
+  output wire [8:0] mem_addr;
+  
+  assign mem_addr = (addr_sel)? pc_out : 8'b00000000;
+  
+endmodule
+
