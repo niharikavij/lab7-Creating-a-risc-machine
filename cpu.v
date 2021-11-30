15baa13596d504e82a39c6fecb1e0d007bd161d3
Miaaauu <89491059+Miaaauu@users.noreply.github.com>
2021-11-29 16:48:51 -0800

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -1,29 +1,26 @@
-module cpu(clk,reset,s,load,in,out,N,V,Z,w);
-input wire clk, reset, s, load;
-input [15:0] in;
-output reg [15:0] out;
-output reg N, V, Z;
-output w;
+module cpu(clk,reset,read_data,mem_cmd,mem_addr);
+input wire clk, reset;
+input [15:0] read_data;
 
-wire w;
+output [1:0] mem_cmd;
+output [8:0] mem_addr;
+
 wire[15:0] datapath_out;
 wire[2:0] Z_out;
-wire write, loada, loadb, loadc, loads, vsel,asel,bsel;
+wire write, loada, loadb, loadc, loads, vsel,asel,bsel, load_ir,reset_pc, load_pc, addr_sel;
 wire [1:0] shift, ALUop,op;
 wire [2:0] nsel,Rd,Rn,Rm,readnum,writenum,opcode;
+wire [8:0] next_pc,pc_out;
 reg [4:0] imm5;
 reg [7:0] imm8;
 wire [15:0] sximm8, sximm5,in_out;
 
-ins_register IR(in,load,clk,in_out);
+ins_register IR(read_data,load_ir,clk,in_out);
 ins_decoder ID(in_out,nsel,sximm5,sximm8,opcode,op,shift,ALUop,readnum,writenum);
-state_machine SM(s,reset,clk, opcode, op, write, w,vsel,loada,loadb,loadc,loads,asel,bsel,nsel);
+state_machine SM(reset,clk,opcode,op,write,vsel,loada,loadb,loadc,loads,asel,bsel,nsel,load_pc,reset_pc,load_ir,addr_sel,mem_cmd);
 datapath DP(sximm8,sximm5,vsel,loada,loadb,shift,bsel,asel,ALUop,loads,loadc,clk,writenum,write,readnum,Z_out,datapath_out);
+Mux_pc MPC(pc_out, reset_pc, next_pc);
+Mux_memory MMEM(pc_out, addr_sel, mem_addr);
+PC PC(next_pc, pc_out, load_pc);
 
-always @(*) begin 
-	N = Z_out[1];
-	V = Z_out[2];
-	Z = Z_out[0];
-	out = datapath_out;
-end 
 endmodule 
